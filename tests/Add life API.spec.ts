d4001c8c3f8b2d57388d08df323b73a2927a433c
Peter Copper <petecopper1980@gmail.com>
2023-07-12 14:02:58 +0100

    initial commit

---
+++ tests/Add life API.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect, request, APIRequestContext } from '@playwright/test';
+
+const loginVerify = require("./common/stuff");
+
+
+test('authenticate', async ({ page }) => {
+    
+  var authtoken = await loginVerify.authorisation();
+
+  const apiRequestContext: APIRequestContext = await request.newContext();
+  
+  var jsonBody = await loginVerify.singleLifeAppJson(); 
+
+  const response = await apiRequestContext.post("https://compare-int-01.underwriteme.co.uk/api/application", {
+      data: jsonBody,
+      headers:{
+        "Content-Type": "application/json",
+        "Authorization":`Bearer ${authtoken}`
+        }
+      });
+
+  //const resBody = await response.json();
+  const responseCode = await response.status();
+  console.log(await response.text());
+
+  await expect(responseCode).toBe(201);
+});
